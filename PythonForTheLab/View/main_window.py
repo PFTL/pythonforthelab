b0e36e6fa2a3344c0b0b1562e874ec0443e85797
Aquiles Carattino <aqui.carattino@gmail.com>
2024-02-18 12:02:16 +0100

    Update to prevent warnings about units. Also displays the last set and get values of voltages

--- PythonForTheLab/View/main_window.py
+++ PythonForTheLab/View/main_window.py
@@ -58,13 +58,18 @@ class MainWindow(QMainWindow):
         self.out_channel_line.setText(str(self.experiment.config['Scan']['channel_out']))
         self.in_channel_line.setText(str(self.experiment.config['Scan']['channel_in']))
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update_plot)
-        self.timer.start(50)
-        self.timer.timeout.connect(self.update_gui)
+        self.gui_timer = QTimer()
+        self.plot_timer = QTimer()
+
+        self.plot_timer.timeout.connect(self.update_plot)
+        self.gui_timer.start(50)
+        self.gui_timer.timeout.connect(self.update_gui)
 
     def update_plot(self):
-        self.plot.setData(self.experiment.scan_range, self.experiment.scan_data)
+        self.plot.setData(self.experiment.scan_range.m_as("V"), self.experiment.scan_data.m_as("V"))
+
+        if not self.experiment.is_running:
+            self.plot_timer.stop()
 
     def start_scan(self):
         start = self.start_line.text()
@@ -84,11 +89,16 @@ class MainWindow(QMainWindow):
              }
         )
         self.experiment.start_scan()
+        self.plot_timer.start(50)
 
     def update_gui(self):
+        self.out_line.setText(f"{self.experiment.voltage_out:3.2f}")
+        self.measured_line.setText(f"{self.experiment.last_measured_value:3.2f}")
+
         if self.experiment.is_running:
             self.start_button.setEnabled(False)
             self.stop_button.setEnabled(True)
+
         else:
             self.start_button.setEnabled(True)
             self.stop_button.setEnabled(False)
