13464dbba6c5e37e1ddbdce20a061dbe897e3a59
Aquiles Carattino <aqui.carattino@gmail.com>
2024-09-11 15:07:39 +0200

    Added space in error message to properly display the message

--- extras/arduino_firmware/arduino_firmware.ino
+++ extras/arduino_firmware/arduino_firmware.ino
@@ -2,7 +2,7 @@
 
 #define IDN_STRING "PFTL DAQ device. Rev 02.2024"
 #define INVALID_CHANNEL_MSG "ERROR: Invalid channel number"
-#define ERROR_COMMAND "ERROR: UNKNOWN COMMAND"
+#define ERROR_COMMAND "ERROR: UNKNOWN COMMAND "
 #define OUT_OF_RANGE_MSG "ERROR: Out of range"
 
 #define DAC_BITS 12
